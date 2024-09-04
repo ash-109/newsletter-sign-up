7f414e2268a10cf5675959258be2b910e5e991f3
Ashvin Antyah <a.antyah@gmail.com>
2024-09-04 17:56:28 +0200

    update html css js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+const container = document.querySelector(".container");
 const form = document.getElementById("form");
 const email = document.getElementById("email");
 const successMessage = document.getElementById("success_message");
 const dissmissMessageBtn = document.getElementById("dissmissMsg_btn");
 const userEmail = document.querySelector(".user_email");
+
+const regexEmail = /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,63}$/;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  userEmail.textContent = email.value;
+  successMessage.style.display = "block";
+  container.style.visibility = "hidden";
+});
+
+dissmissMessageBtn.addEventListener("click", () => {
+  successMessage.style.display = "none";
+  container.style.visibility = "visible";
+  email.value = "";
+});
