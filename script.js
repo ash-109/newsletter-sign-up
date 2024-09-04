786220d4d251ca92762ede5d03680d9b2bfdee49
Ashvin Antyah <a.antyah@gmail.com>
2024-09-04 11:49:44 +0200

    update html & Js

---
+++ script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const successMessage = document.getElementById("success_message");
+const dissmissMessageBtn = document.getElementById("dissmissMsg_btn");
+const userEmail = document.querySelector(".user_email");
