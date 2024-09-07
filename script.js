3a47c4edc6ff1b5ffdb565ad6d823bb19018a811
Ashvin Antyah <a.antyah@gmail.com>
2024-09-07 13:41:13 +0200

    update

--- script.js
+++ script.js
@@ -4,18 +4,29 @@ const email = document.getElementById("email");
 const successMessage = document.getElementById("success_message");
 const dissmissMessageBtn = document.getElementById("dissmissMsg_btn");
 const userEmail = document.querySelector(".user_email");
+email.classList.remove("error");
 
-const regexEmail = /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,63}$/;
+// function validateEmail(email) {
+//   const regexEmail = /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,4}$/;
+//  if (regexEmail.test(email)) {
+//   return true
+//  }
+//  else{
+//   email.classList.add('error')
+//   document.getElementById('errorMessage').style.visibility='visible'
+//  }
+// }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   userEmail.textContent = email.value;
   successMessage.style.display = "block";
   container.style.visibility = "hidden";
+
 });
 
 dissmissMessageBtn.addEventListener("click", () => {
   successMessage.style.display = "none";
   container.style.visibility = "visible";
-  email.value = "";
+  email = "";
 });
